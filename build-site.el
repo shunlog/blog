9eac625acb8c37cb2ca12e65a49a0bbc8de94c00
shunlog <artiombalan331@gmail.com>
2025-09-18 14:06:17 +0300

    first post and homepage sort of done

--- build-site.el
+++ build-site.el
@@ -1,31 +1,68 @@
+(require 'ox-publish)
+(require 'htmlize)
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
-)
+      org-html-link-up ""
+      org-html-link-home "")
 
+(setq org-export-global-macros
+      '(("timestamp" . "@@html:<span class=\"timestamp\">[$1]</span>@@")))
 
-(setq my-website--root
-  (file-name-directory (or load-file-name buffer-file-name)))
+(defun my/org-sitemap-date-entry-format (entry style project)
+  "Format ENTRY in org-publish PROJECT Sitemap format ENTRY ENTRY STYLE format that includes date."
+  (let ((filename (org-publish-find-title entry project)))
+    (if (= (length filename) 0)
+        (format "*%s*" entry)
+      (format "{{{timestamp(%s)}}} [[file:%s][%s]]"
+              (format-time-string "%Y-%m-%d"
+                                  (org-publish-find-date entry project))
+              entry
+              filename))))
 
 
 ;; Define the publishing project
-(setq org-publish-project-alist
-      (list
-       (list "my-org-site"
-             :recursive t
-             :base-directory (expand-file-name "./content" my-website--root)
-             :publishing-directory (expand-file-name "./public" my-website--root)
-             :publishing-function 'org-html-publish-to-html
+(let*
+    ((current-path (file-name-directory (or load-file-name buffer-file-name)))
+     (html-preamble "<nav>
+  <a href=\"/\">&lt; Home</a>
+  <a href=\"/posts\">Posts</a>
+</nav>
+<div id=\"updated\">Updated: %C</div>"))
+  
+  (setq org-publish-project-alist
+        `(("root"
+           :recursive nil
+           :base-extension "org"
+           :base-directory ,(expand-file-name "./content" current-path)
+           :publishing-directory ,(expand-file-name "./public" current-path)
+           :publishing-function org-html-publish-to-html
+           :html-preamble ,html-preamble)
+          
+          ("posts"
+           :recursive t
+           :base-extension "org"
+           :base-directory ,(expand-file-name "./content/posts" current-path)
+           :publishing-directory ,(expand-file-name "./public/posts" current-path)
+           :publishing-function org-html-publish-to-html
+           :html-preamble ,html-preamble
 
-             :with-author nil
-             :with-creator t
-             :with-toc t
-             :section-numbers nil
-             :time-stamp-file t
-             )))
+           :auto-sitemap t
+           :sitemap-filename "index.org"
+           :sitemap-title "Posts"
+           :sitemap-sort-files anti-chronologically
+           :sitemap-format-entry my/org-sitemap-date-entry-format
+
+           :with-toc nil
+           :with-author nil
+           :with-creator t
+           :section-numbers nil
+           :time-stamp-file t)
+
+          ("all"
+           :components ("root" "posts")))))
 
 
-;; Generate the site output
-(org-publish-all t)
+(org-publish "all" 't)
